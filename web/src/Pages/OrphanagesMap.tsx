ef7e292ffa4064ab431556f035264e6620fccf11
mateusamarall <mateus.amaral018@gmail.com>
2020-10-14 08:47:30 -0300

    adicionado mapa na parte web

--- web/src/Pages/OrphanagesMap.tsx
+++ web/src/Pages/OrphanagesMap.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import {Map,TileLayer} from 'react-leaflet';
+import  'leaflet/dist/leaflet.css';
+import mapMarkerImg from '../images/map-marker.svg';
+import '../styles/pages/orphanagesmap.css';
 function OrphanagesMap(){
   return(
-    <h1>hello world</h1>
-  )
+      <div id="page-map">
+        <aside>
+          <header>
+            <img src={mapMarkerImg} alt="Happy"/>
+            <h2>Escolha um orfanato no mapa</h2>
+            <p>Muitas crianças estão esperando a sua visista :) </p>
+          </header>
+          <footer>
+            <strong>Poá</strong>
+            <strong>São Paulo</strong>
+          </footer>
+        </aside>
+        <Map 
+          center={[-23.5175936, -46.3437824]}
+          zoom={15}
+          style={{width:'100%', height:'100%'}}
+        >
+          <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+          </Map>
+
+            <Link to="" className="create-orphanage">
+            <FiPlus size={32} color="#FFF"/>
+            </Link>
+
+      </div>
+  );
 }
 export default OrphanagesMap;
