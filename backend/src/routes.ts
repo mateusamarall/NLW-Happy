2aa7e738af909cfedec7a37b7e45d84f1e2bd8ae
mateusamarall <mateus.amaral018@gmail.com>
2020-10-16 23:31:09 -0300

    adicionado models de orfanato e criado funcionalidade de criar orfanato

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,34 @@
 import express, { response } from 'express';
-
+import {getRepository} from 'typeorm';
+import Orphanage from './models/Orphanage';
 const routes = express.Router();
 
-routes.get('/users',(req,res)=>{
- return res.send('mateus');
+routes.post('/orphanages', async(req,res)=>{
+ const {
+   name,
+   latitude,
+   longitude,
+   about,
+   instructions,
+   opening_hours,
+   open_on_weekends
+} = req.body;
+
+const orphanagesRepository = getRepository(Orphanage);
+
+const orphanage = orphanagesRepository.create({
+  name,
+  latitude,
+  longitude,
+  about,
+  instructions,
+  opening_hours,
+  open_on_weekends
+});
+
+await orphanagesRepository.save(orphanage);
+
+return res.json({message:"created"});
 })
 
 
