b0092f91fe4dcfa1541770ebd6c29ad5bc3bf641
mateusamarall <mateus.amaral018@gmail.com>
2020-10-18 21:05:26 -0300

    criado todas telas no front-end

--- web/src/Pages/OrphanagesMap.tsx
+++ web/src/Pages/OrphanagesMap.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
-import {Map,TileLayer} from 'react-leaflet';
+import {FiPlus,FiArrowRight} from 'react-icons/fi';
+import {Map,TileLayer, Marker, Popup} from 'react-leaflet';
 import  'leaflet/dist/leaflet.css';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanagesmap.css';
+import MapIcon from '../utils/mapIcons';
+
+
 function OrphanagesMap(){
   return(
       <div id="page-map">
@@ -25,9 +28,20 @@ function OrphanagesMap(){
           style={{width:'100%', height:'100%'}}
         >
           <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+          <Marker
+          icon={MapIcon}
+          position={[-23.5175936, -46.3437824]}
+          >
+            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+              Jardim das crianças
+              <Link to="/orphanages/1">
+                  <FiArrowRight size={20} color="#FFF"/>
+              </Link>
+            </Popup>
+          </Marker>
           </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
             <FiPlus size={32} color="#FFF"/>
             </Link>
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Landing from './Pages/Landing';
 import OrphanagesMap from './Pages/OrphanagesMap';
+import Orphanage from './Pages/Orphanage';
+import CreateOrphanage from './Pages/CreateOrphanage';
+
+
 
 function Routes(){
   return(
@@ -9,6 +13,10 @@ function Routes(){
     <Switch>
       <Route exact path="/" component={Landing} />
       <Route path="/app" component={OrphanagesMap} />
+      <Route path="/orphanages/create" component={CreateOrphanage} />
+      <Route path="/orphanages/:id" component={Orphanage} />
+
+
     </Switch>
     </BrowserRouter>
   );
